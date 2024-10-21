7f4ec64d83e4c6378ecc5754ad2828a307491cd4
Parv Mehta <mehtaparv21@gmail.com>
2024-10-21 23:04:09 +0530

    Sign In page done

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,122 @@
-import React from 'react'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable react-hooks/rules-of-hooks */
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import Link from "next/link";
+import { useToast } from "@/hooks/use-toast";
+import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signInSchema } from "@/schemas/signinSchema";
+import { signIn } from "next-auth/react";
+import { useState } from "react";
 
-const page = () => {
+const SignInPage = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { toast } = useToast();
+  const router = useRouter();
+
+  // zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true);
+    const result = await signIn("credentials", {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password,
+    });
+    setIsSubmitting(false);
+
+    if (result?.error) {
+      toast({
+        title: result.error === "CredentialsSignin" ? "Login Failed" : "Error",
+        description:
+          result.error === "CredentialsSignin"
+            ? "Incorrect username or password"
+            : result.error,
+        variant: "destructive",
+      });
+    }
+
+    if (result?.url) {
+      router.replace("/dashboard");
+    }
+  };
+
   return (
-    <div>
-      Sign In Page
+    <div className="flex justify-center items-center min-h-screen bg-green-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Join Mystery Message
+          </h1>
+          <p className="mb-4">Sign up to start your anonymous adventure</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="email/username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="password" type="password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" disabled={isSubmitting}>
+              Sign In
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            New Member?{" "}
+            <Link
+              href={"/signUp"}
+              className="text-blue-600 hover:text-blue-800"
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default SignInPage;
