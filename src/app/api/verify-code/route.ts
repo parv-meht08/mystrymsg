4181c34fb91a8aa99e4b561d40c118c449e90759
Parv Mehta <mehtaparv21@gmail.com>
2024-10-21 19:23:21 +0530

    Verify Page

--- src/app/api/verify-code/route.ts
+++ src/app/api/verify-code/route.ts
@@ -1,68 +1,63 @@
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/User";
-// import { usernameValidation } from "@/schemas/signupSchema";
 
 export async function POST(request: Request) {
   await dbConnect();
 
   try {
     const { username, code } = await request.json();
-
     const decodedUsername = decodeURIComponent(username);
+
     const user = await UserModel.findOne({ username: decodedUsername });
     if (!user) {
-      return Response.json(
-        {
+      return new Response(
+        JSON.stringify({
           success: false,
           message: "User not found",
-        },
+        }),
         { status: 400 }
       );
     }
 
-    const isCodeValid = user.verifyCode === code
-    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date()
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
 
     if (isCodeValid && isCodeNotExpired) {
-        user.isVeryfied = true;
-        await user.save()
+      user.isVeryfied = true;
+      await user.save();
 
-        return Response.json(
-            {
-              success: true,
-              message: "Account veryfied successfully",
-            },
-            { status: 200 }
-          );
-      }
-      else if(!isCodeNotExpired){
-        return Response.json(
-            {
-              success: false,
-              message: "Verification Code has expired please sign up again to get a new code",
-            },
-            { status: 400 }
-          );
-      }
-      else{
-        return Response.json(
-            {
-              success: false,
-              message: "Verification Code is incorrect",
-            },
-            { status: 400 }
-          );
-      }
+      return new Response(
+        JSON.stringify({
+          success: true,
+          message: "Account verified successfully",
+        }),
+        { status: 200 }
+      );
+    } else if (!isCodeNotExpired) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Verification Code has expired, please sign up again to get a new code",
+        }),
+        { status: 400 }
+      );
+    } else {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Verification Code is incorrect",
+        }),
+        { status: 400 }
+      );
+    }
   } catch (error) {
     console.error("Error verifying user", error);
-    return Response.json(
-      {
+    return new Response(
+      JSON.stringify({
         success: false,
         message: "Error verifying user",
-      },
-      {
-        status: 500,
-      }
+      }),
+      { status: 500 }
     );
   }
 }
